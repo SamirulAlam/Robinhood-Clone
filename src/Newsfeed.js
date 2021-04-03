34820deb879d797a876189e09322625770cdd178
Samirul Alam <samirulalam372@gmail.com>
2021-04-04 01:39:07 +0600

    project completed

--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -2,8 +2,24 @@ import React from 'react';
 import LineGraph from './LineGraph';
 import "./Newsfeed.css"
 import TimeLine from './TimeLine';
+import Chip from '@material-ui/core/Chip';
+import { Avatar } from '@material-ui/core';
+
 
 function Newsfeed() {
+
+    const popularTopics=[
+        "Technology",
+        "Top Movies",
+        "Upcoming Earnings",
+        "Crypto",
+        "Cannabis",
+        "Healthcare Supplies",
+        "Index ETFs",
+        "Technology",
+        "China",
+        "Pharma",
+    ]
     return (
         <div className="newsfeed">
             <div className="newsfeed__container">
@@ -26,6 +42,25 @@ function Newsfeed() {
                         <p>Market Closed</p>
                         <h1>Happy Thanksgiving</h1>
                     </div>
+                </div>
+                <div className="newsfeed__popularlists__section">
+                    <div className="newsfeed__popularlists__intro">
+                        <h1>Popular lists</h1>
+                        <p>Show More</p>
+                    </div>
+
+                <div className="newsfeed_popularlists_badges">
+                    {popularTopics.map((topic) => (
+                        <Chip 
+                            className="topic__badge"
+                            variant="outlined"
+                            label={topic}
+                            avatar={<Avatar
+                            src={`https://avatars.dicebear.com/api/human/${topic}.svg`}
+                            />} 
+                        />
+                    ))}
+                </div>
                 </div>
             </div>
         </div>
--- src/StatRow.js
+++ src/StatRow.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import { db } from './firebase';
 import "./StatRow.css";
 import StockSvg from "./stock.svg"
 
 function StatRow(props) {
 
     const percentage = ((props.price-props.openPrice)/props.openPrice) * 100;
+
+    const buyStock =()=>{
+        db.collection("myStocks").where("ticker","==",props.name).get().then((querySnapshot)=>{
+            if(!querySnapshot.empty){
+                querySnapshot.forEach((doc) => {
+                    db.collection("myStocks").doc(doc.id).update({
+                        shares:doc.data().shares+=1
+                    })
+                });
+            }else{
+                db.collection("myStocks").add({
+                    ticker:props.name,
+                    shares:1
+                })
+            }
+        })
+    }
     
 
     return (
-        <div className="statRow">
+        <div className="statRow" onClick={buyStock}>
             <div className="statRow__intro">
                 <h1>{props.name}</h1>
                 <p>{props.shares && (props.shares + " shares")
